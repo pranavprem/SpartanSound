c37475d3ce43fb3c9071a48916b3ace28a83e701
vanarp96 <vanarp96@gmail.com>
2017-04-25 17:44:48 -0700

    added curls to multiple people

--- includes/curls.php
+++ includes/curls.php
@@ -1,11 +1,24 @@
+<html>
+    
+    <head>
 <?php require "head.php"?>
+    </head>
+<body>
+<section class="main-section alabaster" id="tops">
+	<div class="container fullsize">
 <?php
 
 $curl_handle = curl_init();
-curl_setopt($curl_handle, CURLOPT_URL, "localhost/spartansound/includes/expose.php");
+curl_setopt($curl_handle, CURLOPT_URL, "http://www.pranavprem.com/spartansound/includes/expose.php");
 curl_setopt($curl_handle, CURLOPT_HEADER, 0);
 curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true);
 $contents = curl_exec($curl_handle);
+curl_setopt($curl_handle, CURLOPT_URL, "http://divyaatv.com/zyro/fetchuser.php");
+$contents = $contents.",".curl_exec($curl_handle);
+curl_setopt($curl_handle, CURLOPT_URL, "http://hireharry.com/php/userlist/");
+$contents = $contents.",".curl_exec($curl_handle);
+curl_setopt($curl_handle, CURLOPT_URL, "http://helloparag.com/flyby/createDBConn.php");
+$contents = $contents.",".curl_exec($curl_handle);
 
 #echo "<br/>";
 curl_close($curl_handle);
@@ -15,3 +28,8 @@ foreach (explode(",",$contents) as $content) {
 }
 
 ?>
+        
+    </div>
+</section>
+</body>
+</html>
